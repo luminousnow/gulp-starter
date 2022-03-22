e834c88237b78fc438e3dd3a914e0dd9b08c0108
Denis Kravchuk <denis.kravchuk.ua@gmail.com>
2022-03-22 12:45:25 +0200

    fix path, exports

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 import gulp from "gulp";
-import { path } from "./gulp/config/path.js";
-import { plugins } from "./gulp/config/plugins.js";
+import { path } from "./gulp/config/config-path.js";
+import { plugins } from "./gulp/config/config-plugins.js";
 
 // Глобальна змінна
 global.app = {
@@ -12,47 +12,39 @@ global.app = {
 };
 
 // Таски
-import { reset } from "./gulp/tasks/reset.js"; // Очистка каталога .dist
-import { githubDeploy } from "./gulp/tasks/task-githubDeploy.js"; // Деплой на GitHub
-import { server } from "./gulp/tasks/server.js"; // Запуск стріму
-import { html } from "./gulp/tasks/html.js"; // Обробка html
-import { scss } from "./gulp/tasks/scss.js"; // Обробка scss
-import { js } from "./gulp/tasks/js.js"; // Обробка js
-import { img } from "./gulp/tasks/img.js"; // Обробка img
-import { svgSprite } from "./gulp/tasks/svg.js"; // Обробка svgSprite
-
+import { reset } from "./gulp/tasks/task-reset.js"; // Очистка каталога .dist
+import { githubDeploy } from "./gulp/tasks/task-deploy.js"; // Деплой на GitHub
+import { server } from "./gulp/tasks/task-server.js"; // Запуск live Server
+import { html } from "./gulp/tasks/task-html.js"; // Обробка html
+import { scss } from "./gulp/tasks/task-scss.js"; // Обробка scss
+import { javaScript } from "./gulp/tasks/task-javascript.js"; // Обробка js
+import { image } from "./gulp/tasks/task-image.js"; // Обробка img
+import { svgSprite } from "./gulp/tasks/task-svg.js"; // Обробка svgSprite
 import {
   otfToTtf,
   ttfToWoff,
   ttfToWoff2,
   fontsStyle,
-} from "./gulp/tasks/fonts.js";
+} from "./gulp/tasks/task-font.js";
 
 // Спостерігач
 function watcher() {
   gulp.watch(path.watch.html, html);
   gulp.watch(path.watch.scss, scss);
-  gulp.watch(path.watch.js, js);
-  gulp.watch(path.watch.img, img);
+  gulp.watch(path.watch.img, image);
+  gulp.watch(path.watch.js, javaScript);
   gulp.watch(path.watch.svg, svgSprite);
 }
 
-// Сценарії Gulp
-const fonts = gulp.series(otfToTtf, ttfToWoff, ttfToWoff2, fontsStyle); // Конвертація шрифтів
+// Сценарії Gulp конвертація шрифтів
 const mainTasks = gulp.series(
-  fonts,
-  gulp.parallel(html, scss, js, img, svgSprite)
+  gulp.series(otfToTtf, ttfToWoff, ttfToWoff2, fontsStyle),
+  gulp.parallel(html, scss, javaScript, image, svgSprite)
 );
 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(server, watcher));
 const build = gulp.series(reset, mainTasks);
 const deploy = gulp.series(build, githubDeploy);
 
-// Експорт
+// Експорт для NPM
 export { dev, build, deploy, reset };
-
-// Завдання Gulp
-gulp.task("default", dev);
-gulp.task("build", build);
-gulp.task("deploy", deploy);
-gulp.task("reset", reset);
