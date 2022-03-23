86c7e7e232cd636381506986b1e11bf697e5f16e
Denis Kravchuk <denis.kravchuk.ua@gmail.com>
2022-03-23 21:47:36 +0200

    add comments

--- gulp/config/config-plugins.js
+++ gulp/config/config-plugins.js
@@ -1,7 +1,7 @@
 import replace from "gulp-replace"; // Пошук та заміна
 import browserSync from "browser-sync"; // Трансляція, оновлення сторінки браузера
 import newer from "gulp-newer"; // Перевірка на нові зображення
-import gulpIf from "gulp-if";
+import gulpIf from "gulp-if"; // Виконує дію по умові
 
 export const plugins = {
   replace,
