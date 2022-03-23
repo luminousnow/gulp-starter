9a0f20bf17f6a140093b77f4a80d16ae486b0fd4
Denis Kravchuk <denis.kravchuk.ua@gmail.com>
2022-03-23 21:01:41 +0200

    add default fonts Roboto

--- gulp/tasks/task-deploy.js
+++ gulp/tasks/task-deploy.js
@@ -1,5 +1,6 @@
 import ghpages from "gulp-gh-pages";
 
 export const githubDeploy = async () => {
-  app.gulp.src(`${app.path.buildFolder}/**/*.*`).pipe(ghpages());
+  // app.gulp.src(`${app.path.buildFolder}/**/*.*`).pipe(ghpages());
+  app.gulp.src(app.path.buildFolder).pipe(ghpages());
 };
