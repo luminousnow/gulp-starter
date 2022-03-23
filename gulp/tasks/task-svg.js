a5e3240ee39e9d21935689a64bf0871a661eacf7
Denis Kravchuk <denis.kravchuk.ua@gmail.com>
2022-03-23 23:16:05 +0200

    fix task svg, add cheerio plugin

--- gulp/tasks/task-svg.js
+++ gulp/tasks/task-svg.js
@@ -1,8 +1,21 @@
 import svgsprite from "gulp-svg-sprite";
+import cheerio from "gulp-cheerio";
 
 export const svgSprite = () => {
   return app.gulp
-    .src(app.path.src.svg)
+    .src(app.path.src.svg, {})
+    .pipe(
+      // прибираємо зайві стилі щоб вони не перебивали css
+      cheerio({
+        run: function ($) {
+          $("[fill]").removeAttr("fill");
+          $("[stroke]").removeAttr("stroke");
+          $("[style]").removeAttr("style");
+        },
+        parserOptions: { xmlMode: true },
+      })
+    )
+    .pipe(app.plugins.replace("&gt;", ">")) // фіксим баг роботи cheerio
     .pipe(
       svgsprite({
         mode: {
@@ -13,7 +26,11 @@ export const svgSprite = () => {
         },
       })
     )
-    .pipe(app.gulp.dest(`${app.path.sourceFolder}/assets/img/svg`))
-    .pipe(app.gulp.src(app.path.src.svg))
+    .pipe(app.gulp.dest(`${app.path.sourceFolder}/assets/img/svg/`))
+    .pipe(
+      app.gulp.src(`${app.path.sourceFolder}/assets/img/svg/icons.svg`, {
+        allowEmpty: true,
+      })
+    )
     .pipe(app.gulp.dest(app.path.build.svg));
 };
