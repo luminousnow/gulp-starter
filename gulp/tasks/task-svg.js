66c71df4014d5659dfbfbb272bdabbc5f14e6e9f
Denis Kravchuk <denis.kravchuk.ua@gmail.com>
2022-03-25 09:51:17 +0200

    add cheerio optimisation

--- gulp/tasks/task-svg.js
+++ gulp/tasks/task-svg.js
@@ -2,35 +2,43 @@ import svgsprite from "gulp-svg-sprite";
 import cheerio from "gulp-cheerio";
 
 export const svgSprite = () => {
-  return app.gulp
-    .src(app.path.src.svg, {})
-    .pipe(
-      // прибираємо зайві стилі щоб вони не перебивали css
-      cheerio({
-        run: function ($) {
-          $("[fill]").removeAttr("fill");
-          $("[stroke]").removeAttr("stroke");
-          $("[style]").removeAttr("style");
-        },
-        parserOptions: { xmlMode: true },
-      })
-    )
-    .pipe(app.plugins.replace("&gt;", ">")) // фіксим баг роботи cheerio
-    .pipe(
-      svgsprite({
-        mode: {
-          stack: {
-            sprite: `../icons.svg`,
-            example: false, // додати файл з виглядом іконок
+  return (
+    app.gulp
+      .src(app.path.src.svg, {})
+
+      // прибираємо зайві стилі, щоб вони не перебивали css
+      .pipe(
+        cheerio({
+          run: function ($) {
+            $("[fill]").removeAttr("fill");
+            $("[stroke]").removeAttr("stroke");
+            $("[style]").removeAttr("style");
           },
-        },
-      })
-    )
-    .pipe(app.gulp.dest(`${app.path.sourceFolder}/assets/img/svg/`))
-    .pipe(
-      app.gulp.src(`${app.path.sourceFolder}/assets/img/svg/icons.svg`, {
-        allowEmpty: true,
-      })
-    )
-    .pipe(app.gulp.dest(app.path.build.svg));
+          parserOptions: { xmlMode: true },
+        })
+      )
+      .pipe(app.plugins.replace("&gt;", ">")) // фіксим баг роботи cheerio
+
+      // створюємо спрайт
+      .pipe(
+        svgsprite({
+          mode: {
+            stack: {
+              sprite: `../icons.svg`,
+              example: false, // додати файл з виглядом іконок
+            },
+          },
+        })
+      )
+      // вигружаємо тимчасово в src
+      .pipe(app.gulp.dest(`${app.path.sourceFolder}/assets/img/svg/`))
+      // забераэмо з src
+      .pipe(
+        app.gulp.src(`${app.path.sourceFolder}/assets/img/svg/icons.svg`, {
+          allowEmpty: true,
+        })
+      )
+      // кладемо в dist
+      .pipe(app.gulp.dest(app.path.build.svg))
+  );
 };
